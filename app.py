3df199272b6e5ccdb36ef1fd7fa20992994f0520
Anurag <kranurag2004@gmail.com>
2025-07-26 02:39:17 +0530

    update app.py

--- app.py
+++ app.py
@@ -66,4 +66,5 @@ def predict():
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # use PORT from env, default to 5000 locally
+    app.run(host='0.0.0.0', port=port)
